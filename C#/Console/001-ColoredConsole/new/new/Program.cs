d36d206f06d70b9b7bd4efa215b87645409cedb8
ZelvaMan <honza.rada1@seznam.cz>
2019-05-08 22:07:57 +0200

    Update Program.cs

--- C#/Console/001-ColoredConsole/new/new/Program.cs
+++ C#/Console/001-ColoredConsole/new/new/Program.cs
@@ -10,14 +10,14 @@ namespace @new
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
             Console.Title = "Konecne funkcni";
             Console.WriteLine("Zadejte datum ve formatu dd mm rrrrr s mezerami mezi jednotlivymi cisly");
             Models.Date date = ParseTheDate();
-            
-            
+
+
             int CurDay = date.Day;
 
             int curMonth = date.Month;
@@ -29,9 +29,9 @@ namespace @new
             ConsoleColor color = GetSeason(date.Day, date.Month);
             while (true)
             {
-                
-                
-                
+
+
+
 
                 Console.ForegroundColor = color;
                 Console.WriteLine($"{CurDay} {curMonth} {curYear}");
@@ -75,8 +75,8 @@ namespace @new
                 if (CurDay == 21 && curMonth == 12)
                     color = ConsoleColor.Blue;
 
-                
-                if(curYear >= date.Year + 5 && curMonth == date.Month && CurDay == date.Day)
+
+                if (curYear >= date.Year + 5 && curMonth == date.Month && CurDay == date.Day)
                 {
                     Console.WriteLine($"{CurDay} {curMonth} {curYear}");
                     break;
@@ -117,36 +117,55 @@ namespace @new
             while (true)
             {
                 string text = Console.ReadLine();
-                int _year, _month, _day;
+                int _year = 0, _month = 0, _day = 0;
                 List<string> validationMesseges = new List<string>();
+                validationMesseges.Clear();
                 string[] spl = text.Split(' ');
                 if (int.TryParse(spl[0], out _day) && int.TryParse(spl[1], out _month) && int.TryParse(spl[2], out _year))
+                {
+                    if (_month < 0 || _month > 13)
+                        validationMesseges.Add("Neplatny mesic, mesicu muze byt jen 12");
+                    if (_day < 0 || _day > GetNumberOfDays(_month, (_year % 4) == 0))
+                        validationMesseges.Add($"Nelatny pocet dnu, v tomto mesici je jen {GetNumberOfDays(_month, (_year % 4) == 0)} dnu");
+                    if (_year < 1950 || _year > 2050)
+                        validationMesseges.Add("Neplatny rok, roky muzou byt jen v rozpeti 1950 az 2050");
 
-                    
+                }
+                else validationMesseges.Add("nevalidni datum ");
+                //pokud se nanaskitla zadna chyba
+                if( validationMesseges.Count == 0)
+                    return new Models.Date(_year, _month, _day);
+                //pokud se chybz naskytli vypise
+                Console.WriteLine("Pri validaci data nastaly nasledujici chyby");
+                foreach(string s in validationMesseges)
+                {
+                    Console.Write(s);
+                }
+                Console.WriteLine("")
+                
 
 
-                            
-                            return new Models.Date(_year, _month, _day);
-                       
+
+
 
             }
-            
+
         }
         public static ConsoleColor GetSeason(int day, int month)
         {
             //winter 
             if (month == 1 && month == 2 &&
-                (month == 12 && day > 22) && 
+                (month == 12 && day > 22) &&
                 (month == 3 && day <= 20))
                 return ConsoleColor.Blue;
             //spring 
             if (month == 4 && month == 5 &&
-                (month == 3 && day >= 22 ) && 
+                (month == 3 && day >= 22) &&
                 (month == 6 && day < 21))
                 return ConsoleColor.Green;
             //summer 
             if (month == 7 && month == 8 &&
-                (month == 6 && day >= 21) && 
+                (month == 6 && day >= 21) &&
                 (month == 9 && day < 23))
                 return ConsoleColor.Red;
             //fall
