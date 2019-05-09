d8f1d8249c2203b1432f471053b570cf25a7d0cb
ZelvaMan <honza.rada1@seznam.cz>
2019-05-09 23:01:38 +0200

    Program v3 FULL FUNCTION
    plne funkcni

--- C#/Console/001-ColoredConsole/new/new/Models/Date.cs
+++ C#/Console/001-ColoredConsole/new/new/Models/Date.cs
@@ -11,25 +11,55 @@ namespace @new.Models
         public int Year { get; set; }
         public int Month { get; set; }
         public int Day { get; set; }
+        public bool LeapYear
+        {
+            get
+            {
+                return (Year % 4) == 0;
+            }
+        }
 
-        public Date(int year, int month, int day )
+        public Date(int year, int month, int day)
         {
             Year = year;
             Month = month;
             Day = day;
-
         }
 
-        public bool Leap_year()
+        public Date() { }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="month"></param>
+        /// <param name="leapYear"></param>
+        /// <returns></returns>
+        public int GetNumberOfDays(bool leapYear)
         {
-            return (Year % 4) == 0;
+            switch (Month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+
+                    return 31;
+                    break;
+                case 2:
+                    return leapYear ? 29 : 28;
+                    break;
+
+            }
+            return 30;
         }
 
         /// <summary>
         /// Parsuje datum z konzole ve formatu DD MM YYYY
         /// </summary>
         /// <returns> datum s rokem , mesicem a dnem</returns>
-        public static Models.Date ParseTheDate()
+        public void ParseTheDate()
         {
             // 1950 - 2050 rok
             while (true)
@@ -41,18 +71,23 @@ namespace @new.Models
                 string[] spl = text.Split(' ');
                 if (int.TryParse(spl[0], out _day) && int.TryParse(spl[1], out _month) && int.TryParse(spl[2], out _year))
                 {
-                    if (_month < 0 || _month > 13)
-                        validationMesseges.Add("Neplatny mesic, mesicu muze byt jen 12");
-                    if (_day < 0 || _day > Program.GetNumberOfDays(_month, (_year % 4) == 0))
-                        validationMesseges.Add($"Nelatny pocet dnu, v tomto mesici je jen {Program.GetNumberOfDays(_month, (_year % 4) == 0)} dnu");
                     if (_year < 1950 || _year > 2050)
                         validationMesseges.Add("Neplatny rok, roky muzou byt jen v rozpeti 1950 az 2050");
 
+                    if (_month < 0 || _month > 13)
+                        validationMesseges.Add("Neplatny mesic, mesicu muze byt jen 12");
+
+                    if (_day < 0 || _day > GetNumberOfDays((_year % 4) == 0))
+                        validationMesseges.Add($"Nelatny pocet dnu, v tomto mesici je jen {GetNumberOfDays(LeapYear)} dnu");
+
+                    Year = _year;
+                    Month = _month;
+                    Day = _day;
                 }
                 else validationMesseges.Add("nevalidni datum ");
                 //pokud se nanaskitla zadna chyba
                 if (validationMesseges.Count == 0)
-                    return new Models.Date(_year, _month, _day);
+                    return;
                 //pokud se chybz naskytli vypise
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Pri validaci data nastaly nasledujici chyby");
@@ -64,16 +99,36 @@ namespace @new.Models
                 }
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("zadejte prosim znovu datum ve formatu DD MM YYYY ");
-
-
-
-
-
-
             }
 
         }
 
+        /// <summary>
+        /// Vrati rocni obdobi na zaklade data
+        /// </summary>
+        /// <param name="Day">den </param>
+        /// <param name="Month">mesic </param>
+        /// <returns> barvu zastupujici rocni obdobi</returns>
+        public ConsoleColor GetSeason()
+        {
+            //winter 
+            if (Month == 1 && Month == 2 &&
+                (Month == 12 && Day > 22) &&
+                (Month == 3 && Day <= 20))
+                return ConsoleColor.Blue;
+            //spring 
+            if (Month == 4 && Month == 5 &&
+                (Month == 3 && Day >= 22) &&
+                (Month == 6 && Day < 21))
+                return ConsoleColor.Green;
+            //summer 
+            if (Month == 7 && Month == 8 &&
+                (Month == 6 && Day >= 21) &&
+                (Month == 9 && Day < 23))
+                return ConsoleColor.Red;
+            //fall
+            return ConsoleColor.Magenta;
+        } 
         public override string ToString()
         {
             return $"{Day.ToString()} {Month.ToString()} {Year.ToString()}";
--- C#/Console/001-ColoredConsole/new/new/Program.cs
+++ C#/Console/001-ColoredConsole/new/new/Program.cs
@@ -13,39 +13,46 @@ namespace @new
 
         static void Main(string[] args)
         {
+
             Console.Title = "Konecne funkcni";
             Console.WriteLine("Zadejte datum ve formatu dd mm rrrrr s mezerami mezi jednotlivymi cisly");
-            Models.Date date = Models.Date.ParseTheDate();
+            Models.Date date = new Models.Date();
+            date.ParseTheDate();
 
+            //presetproperties
             Models.Date curentDate = new Models.Date(date.Year - 5, date.Month, date.Day);
-            bool leap_year = curentDate.Leap_year();
-            int DayOfMonth = GetNumberOfDays(curentDate.Year, leap_year);
-            ConsoleColor color = GetSeason(date.Day, date.Month);
+            bool leap_year = curentDate.LeapYear;
+            int DayOfMonth = curentDate.GetNumberOfDays(leap_year);
+            ConsoleColor color = curentDate.GetSeason();
             while (true)
             {
 
 
 
-
+                //set color to color of seasn
                 Console.ForegroundColor = color;
+                //write a a date
                 Console.WriteLine(curentDate.ToString());
 
                 curentDate.Day++;
 
+                //end of the month
                 if (curentDate.Day >= DayOfMonth + 1)
                 {
-                    DayOfMonth = GetNumberOfDays(curentDate.Month, leap_year);
+                    DayOfMonth = curentDate.GetNumberOfDays(leap_year);
                     curentDate.Day = 1;
                     curentDate.Month++;
                 }
 
-                //konec roku
+                //end of the year
                 if (curentDate.Month >= 12 && curentDate.Day >= DayOfMonth)
                 {
+                    
                     curentDate.Month = 1;
                     curentDate.Year++;
                     curentDate.Day = 1;
-                    leap_year = curentDate.Leap_year();
+                    leap_year = curentDate.LeapYear;
+                    //if leap year write yelow  line 
                     if (leap_year) 
                     {
                         Console.ForegroundColor = ConsoleColor.Yellow;
@@ -53,23 +60,23 @@ namespace @new
                         Console.ForegroundColor = ConsoleColor.White;
                     }
                 }
-                //jaro
+                //spring
                 if (curentDate.Day == 20 && curentDate.Month == 3)
                     color = ConsoleColor.Green;
 
-                //leto
+                //summer
                 if (curentDate.Day == 21 && curentDate.Month == 6)
                     color = ConsoleColor.Red;
 
-                //podzim
+                //fall
                 if (curentDate.Day == 23 && curentDate.Month == 9)
                     color = ConsoleColor.Magenta;
 
-                //zima
+                //winter
                 if (curentDate.Day == 21 && curentDate.Month == 12)
                     color = ConsoleColor.Blue;
 
-
+                //end of ciclus
                 if (curentDate.Year >= date.Year + 5 && curentDate.Month == date.Month && curentDate.Day == date.Day)
                 {
                     Console.WriteLine(curentDate.ToString());
@@ -78,60 +85,9 @@ namespace @new
             }
             Console.ReadKey();
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="month"></param>
-        /// <param name="leapYear"></param>
-        /// <returns></returns>
-        public static int GetNumberOfDays(int month, bool leapYear)
-        {
-            switch (month)
-            {
-                case 1:
-                case 3:
-                case 5:
-                case 7:
-                case 8:
-                case 10:
-                case 12:
-
-                    return 31;
-                    break;
-                case 2:
-                    return leapYear ? 29 : 28;
-                    break;
-
-            }
-            return 30;
-        }
+        
 
         
-        /// <summary>
-        /// Vrati rocni obdobi na zaklade data
-        /// </summary>
-        /// <param name="day">den </param>
-        /// <param name="month">mesic </param>
-        /// <returns> barvu zastupujici rocni obdobi</returns>
-        public static ConsoleColor GetSeason(int day, int month)
-        {
-            //winter 
-            if (month == 1 && month == 2 &&
-                (month == 12 && day > 22) &&
-                (month == 3 && day <= 20))
-                return ConsoleColor.Blue;
-            //spring 
-            if (month == 4 && month == 5 &&
-                (month == 3 && day >= 22) &&
-                (month == 6 && day < 21))
-                return ConsoleColor.Green;
-            //summer 
-            if (month == 7 && month == 8 &&
-                (month == 6 && day >= 21) &&
-                (month == 9 && day < 23))
-                return ConsoleColor.Red;
-            //fall
-            return ConsoleColor.Magenta;
-        }
+
     }
 }
